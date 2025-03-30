051759854b68ef9cb9e48717ab57fbc703cca2e7
Aditya Gandhi <aditya.g8@gmail.com>
2025-03-30 03:19:34 -0400

    Enable snack
    - Disable fzf
    - Enable Snack explorer
    - Enable Snack picker
    - Enable Snack image

---
+++ lua/plugins/snack.lua
@@ -0,0 +1,10 @@
+return {
+  -- change trouble config
+  {
+    "folke/snacks.nvim",
+    -- opts will be merged with the parent spec
+    opts = {
+      image = { enabled = true },
+    },
+  },
+}
